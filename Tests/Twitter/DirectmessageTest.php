6083c7f521e47849378abab87f07f63f13063c0b
ricomonster <riconess@gmail.com>
2013-10-10 12:56:42 +0800

    done testing direct message

--- Tests/Twitter/DirectmessageTest.php
+++ Tests/Twitter/DirectmessageTest.php
@@ -10,116 +10,116 @@
 class Eden_Twitter_Tests_Twitter_DirectmessageTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp() {
-        $this->consumerKey = 'QwgkQh3AiloLxQ9PcjqNgA';
-        $this->consumerSecret = 'LNOxfFGIWTzlKSpL23qY28kxLxfwfKBDW3vh1BSw4M';
+        $this->consumerKey = 'OanO9yIXRLPeNtjiywdHow';
+        $this->consumerSecret = 'vyOvpmTNH2GCcdATJV6J0n1iT53uPYy3cgDNsgBPydY';
 
-        $this->accessToken = '21862667-Prmb3MOGBqFhqtjf2AlHDcq8MwdSocE4t2i1k3DBB';
-        $this->accessSecret = 'jTAEUuy9cSinM5UdAeh345RCjoy0dA7JtYsqzBj9M0';
+        $this->accessToken = '';
+        $this->accessSecret = '';
 
-        $this->messageId = '377822016369201152';
-        $this->id = 'ricocomonster';
-        $this->text = 'unit testing api';
-        $this->count = 5;
-        $this->maxId = '384625453299806208';
-        $this->page = '2';
-        $this->sinceId = '384625453299806208';
+        $this->messageId = '';
+        $this->id = '';
+        $this->text = '';
+        $this->count = 0;
+        $this->maxId = '';
+        $this->page = '';
+        $this->sinceId = '';
         $this->wrap = true;
     }
 
     public function testGetList()
     {
-        // $list = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->getList();
-        // $this->assertTrue('Eden\\Twitter\\Directmessage', !isset($list['errors']));
+        $list = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->getList();
+        print_r($list);
+        $this->assertArrayHasKey('id', $list[0]);
     }
 
     public function testGetSent()
     {
-        // $sent = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->getSent();
-        // $this->assertTrue('Eden\\Twitter\\Directmessage', !isset($sent['errors']));
+        $sent = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->getSent();
+        $this->assertArrayHasKey('id', $sent[0]);
     }
 
     public function testGetDetail()
     {
-        // $detail = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->getDetail($this->messageId);
-        // $this->assertTrue('Eden\\Twitter\\Directmessage', !isset($detail['errors']));
+        $detail = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->getDetail($this->messageId);
+        $this->assertArrayHasKey('id', $detail);
     }
 
     public function testRemove()
     {
-        // $remove = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->remove($this->id);
-        // print_r($remove);
-        // $this->assertTrue('Eden\\Twitter\\Directmessage', !isset($remove['errors']));
+        $remove = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->remove($this->messageId);
+        $this->assertArrayHasKey('sender_id_str', $remove);
     }
 
     public function testSend()
     {
-        // $send = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->send($this->id, $this->text);
-        // $this->assertTrue('Eden\\Twitter\\Directmessage', !isset($send['errors']));
+        $send = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->send($this->id, $this->text);
+        $this->assertArrayHasKey('sender_id', $send);
     }
 
     public function testIncludeEntities()
     {
-        // $entities = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->includeEntities();
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $entities);
+        $entities = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->includeEntities();
+        $this->assertObjectHasAttribute('query', $entities);
     }
 
     public function testSetCount()
     {
-        // $count = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->setCount($this->count);
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $count);
+        $count = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->setCount($this->count);
+        $this->assertObjectHasAttribute('query', $count);
     }
 
     public function testSetMaxId()
     {
-        // $maxId = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->setMaxId($this->count);
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $maxId);
+        $maxId = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->setMaxId($this->count);
+        $this->assertObjectHasAttribute('query', $maxId);
     }
 
     public function testSetPage()
     {
-        // $page = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->setPage($this->page);
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $page);
+        $page = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->setPage($this->page);
+        $this->assertObjectHasAttribute('query', $page);
     }
 
     public function testSetSinceId()
     {
-        // $since = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->setSinceId($this->sinceId);
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $since);
+        $since = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->setSinceId($this->sinceId);
+        $this->assertObjectHasAttribute('query', $since);
     }
 
     public function testSetWrap()
     {
-        // $wrap = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->setWrap($this->wrap);
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $wrap);
+        $wrap = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->setWrap($this->wrap);
+        $this->assertObjectHasAttribute('query', $wrap);
     }
 
     public function testSkipStatus()
     {
-        // $skip = eden('twitter')
-        //     ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
-        //     ->skipStatus($this->count);
-        // $this->assertInstanceOf('Eden\\Twitter\\Directmessage', $skip);
+        $skip = eden('twitter')
+            ->directMessage($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessSecret)
+            ->skipStatus($this->count);
+        $this->assertObjectHasAttribute('query', $skip);
     }
 }
